8513a3664708897f0e828a3f8bdd67c3616eba10
Ralf van den Broek <ralf@ralfvandenbroek.nl>
2017-11-02 17:49:28 +0100

    Replace wit parser

--- GroningerAPI/wit_parser.py
+++ GroningerAPI/wit_parser.py
@@ -1,4 +1,4 @@
-import urllib2
+import urllib
 import json
 
 WIT_ACCESS_TOKEN='GE3WFS7X5FGREBMX7WA46XIHMO2ES7WC'
@@ -7,8 +7,9 @@ WIT_API_VERSION = '20160516'
 
 class WitParser:
     def parse(self, message):
-        req = urllib2.Request(WIT_API_HOST + '/message?q=' + urllib2.urlencode(message))
-        req.add_header('authorization', 'Bearer ' + WIT_ACCESS_TOKEN)
-        req.add_header('accept', 'application/vnd.wit.' + WIT_API_VERSION + '+json')
-        resp = urllib2.urlopen(req)
+        req = urllib.request.Request(url=WIT_API_HOST + '/message?q=' + urllib2.urlencode(message), headers={
+            'authorization': 'Bearer ' + WIT_ACCESS_TOKEN,
+            'accept': 'application/vnd.wit.' + WIT_API_VERSION + '+json'
+        })
+        resp = urllib.urlopen(req)
         return json.loads(resp.read())
