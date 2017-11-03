2a648e0535478663d4cd1762db79653f8bd35fdd
Sergen Nurel <sergen_nurel@hotmail.com>
2017-11-03 01:12:20 +0100

    fix?

--- GroningerAPI/conversation_handler.py
+++ GroningerAPI/conversation_handler.py
@@ -1,3 +1,5 @@
+from datetime import timedelta, datetime
+
 from GroningerAPI.intentparser import IntentParser
 from GroningerAPI.models import Message, Conversation, User
 from GroningerAPI.wit_parser import WitParser
@@ -5,20 +7,20 @@ from GroningerAPI.wit_parser import WitParser
 class ConversationHandler:
     def receive_message(self, message_text, user_data):
         try:
-            conversation = None
-            # beetje dodgy
             if 'first_name' not in user_data:
                 user, created = User.objects.get_or_create(session_id=user_data['id'])
             else:
                 user, created = User.objects.get_or_create(facebook_id=user_data['id'], name=user_data['first_name'], surname=user_data['last_name'])
             print(user)
 
+            yesterday = datetime.today() - timedelta(days=1)
+            #conversation = Conversation.objects.get_or_create(sender=user, time_stamp__range=(yesterday, datetime.today()))
+            conversation = conversation = Conversation.objects.order_by("-time_stamp").first()
+
             if not conversation:
                 conversation = Conversation(user=user)
                 conversation.save(force_insert=True)
-            else:
-                conversation = Conversation.objects.order_by("-time_stamp").first()
-            print(conversation)
+
 
             Message.objects.create(conversation=conversation, type="message", sender="user", data=message_text)
 
